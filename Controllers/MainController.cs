9096e5e0cf32db111b63011379cb654d12a3869d
Mark Lechtermann <mark.lechtermann@gmail.com>
2022-07-23 00:42:24 +0200

    prerelease tag

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -15,7 +15,7 @@ public class MainController : ControllerBase
         {
             Version = informationalVersion?.InformationalVersion,
             GitSHA = git_sha,
-            Description = "Hello 123"
+            Description = "Hello 1234"
         };
     }
 }
