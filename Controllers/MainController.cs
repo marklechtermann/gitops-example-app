b649227905cc880e5884b2315b914153575a845d
Mark Lechtermann <mark.lechtermann@gmail.com>
2022-08-15 18:07:06 +0200

    new version

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -15,7 +15,7 @@ public class MainController : ControllerBase
         {
             Version = informationalVersion?.InformationalVersion,
             GitSHA = git_sha,
-            Description = "Hello 12345",
+            Description = "Hello 1234567",
             Secret = Environment.GetEnvironmentVariable("MYSECRET")
         };
     }
