b1cd0b99bfa4d61ebf46468604d98cdd8ea0aa8d
Mark Lechtermann <mark.lechtermann@gail.com>
2022-09-01 08:54:24 +0200

    chore: code change

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -15,7 +15,7 @@ public class MainController : ControllerBase
         {
             Version = informationalVersion?.InformationalVersion,
             GitSHA = git_sha,
-            Description = "Hello 123 dev",
+            Description = "Hello 123 dev 123",
             Secret = Environment.GetEnvironmentVariable("MYSECRET")
         };
     }
